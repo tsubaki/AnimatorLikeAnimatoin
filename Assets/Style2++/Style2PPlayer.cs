6189eda7c8448ad60fd670d814855b50eeabb441
tsubaki <ruitomo.ex@gmail.com>
2016-05-27 22:41:45 +0900

    BlendTreeの2Dアニメーションを追加

---
+++ Assets/Style2++/Style2PPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Style2PPlayer : MonoBehaviour
+{
+	void Update ()
+	{
+	
+		Vector2 velocity = GetComponent<Rigidbody2D> ().velocity;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			velocity.y = 5;
+		}
+
+		velocity.x = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = velocity;
+	}
+}
---
+++ Assets/Style2++/Style2PlayerBehavour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Style2PlayerBehavour : StateMachineBehaviour
+{
+	[SerializeField]
+	LayerMask mask;
+
+	static int ySpeedHash = Animator.StringToHash ("y speed");
+	static int xSpeedHash = Animator.StringToHash ("x speed");
+	static int distanceFromGround = Animator.StringToHash ("distance from ground");
+	static int landingTimeHash = Animator.StringToHash ("landing time");
+
+	public float landingTime = 0;
+
+	override public void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		var transform = animator.transform;
+		var rigidbody2d = animator.GetComponent<Rigidbody2D> ();
+		var hit = Physics2D.Raycast (transform.position, transform.up * -1, 5, mask);
+
+		if (hit.distance > 0.6f) {
+			landingTime = 1f;
+		}
+
+		if (rigidbody2d.velocity.x != 0) {
+			landingTime = 0;
+		}
+		landingTime -= Time.deltaTime;
+
+		animator.SetFloat (xSpeedHash, Mathf.Abs (rigidbody2d.velocity.x));
+		animator.SetFloat (ySpeedHash, rigidbody2d.velocity.y);
+		animator.SetFloat (distanceFromGround, hit.distance);
+		animator.SetFloat (landingTimeHash, landingTime);
+
+		if (rigidbody2d.velocity.x != 0) {
+			animator.GetComponent<SpriteRenderer> ().flipX = rigidbody2d.velocity.x < 0;
+		}
+	}
+}
